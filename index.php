dded51855c21a7203ea8a5aefd4a90352b9b042e
Marco Valerio <mo.valerio93@gmal.com>
2022-09-22 14:21:57 +0200

    php-oop-1 construct defined

--- index.php
+++ index.php
@@ -3,8 +3,24 @@
 class Movie {
 
     public $title;
-    public $tipe;
+    public $type;
     public $year;
 
+    public function __construct($argTitle,$argType,$argYear = null)
+    {
+        $this->title = $argTitle;
+        $this->type = $argType;
+        $this->year = $argYear;
+        
+    }
 
 }
+
+
+$iMovie = new Movie ("2001: a space odissey","Science fiction","1968");
+
+$iMovie = new Movie ("The Hitchhiker's guide to the galaxy","Science fiction","2005");
+
+$iMovie = new Movie ("The Hangover","Commedy","2009");
+
+var_dump($iMovie);
