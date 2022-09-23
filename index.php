0fc6a8d36885b7d4c0e753c2f4e5a357940e116c
Marco Valerio <mo.valerio93@gmal.com>
2022-09-23 10:00:10 +0200

    php-opp-1  methods defined

--- index.php
+++ index.php
@@ -4,23 +4,36 @@ class Movie {
 
     public $title;
     public $type;
+     public $director;
     public $year;
 
-    public function __construct($argTitle,$argType,$argYear = null)
+    public function __construct($argTitle,$argType,$argDirector = null)
     {
         $this->title = $argTitle;
         $this->type = $argType;
-        $this->year = $argYear;
-        
+        $this->director= $argDirector;
     }
+
+    public function setYear($year) {
+
+        if (is_int($year) && $year >= 1895&& $year < 2030) {
+            $this->year =$year;
+        } else {
+            die('Errore anno!');
+        }
+    }
+    public function getYear(){
+        return $this->year;
+    }
+
 
 }
 
 
-$iMovie = new Movie ("2001: a space odissey","Science fiction","1968");
+$firstMovie = new Movie ("2001: a space odissey","Science fiction","1968");
 
-$iMovie = new Movie ("The Hitchhiker's guide to the galaxy","Science fiction","2005");
+$secondMovie = new Movie ("The Hitchhiker's guide to the galaxy","Science fiction","2005");
 
-$iMovie = new Movie ("The Hangover","Commedy","2009");
+$thirdMovie = new Movie ("The Hangover","Commedy","2009");
 
 var_dump($iMovie);
